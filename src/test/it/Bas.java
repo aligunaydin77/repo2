7916a5fa38f8e508fb58c4533d8427fca9f0c7f4
Gunaydin, Ali (Developer) <ali.gunaydin@sky.uk>
2016-05-06 09:15:21 +0100

    dev goes and and this went to dev branch

--- src/test/it/Bas.java
+++ src/test/it/Bas.java
@@ -3,6 +3,7 @@ package test.it;
 public class Bas implements Singer {
 
 	// thats a featurea not a bug!!!
+	//dev goes and added that
 	@Override
 	public void sing() {
 		System.out.println("bas sings");

88e91c1624d4ea30c22a0de7caccc4d47064bcde
Gunaydin, Ali (Developer) <ali.gunaydin@sky.uk>
2016-05-06 09:13:51 +0100

    forking featureb from dev

--- src/test/it/Bas.java
+++ src/test/it/Bas.java
@@ -3,6 +3,7 @@ package test.it;
 public class Bas implements Singer {
 
 	// thats a featurea not a bug!!!
+	//thats the featureb
 	@Override
 	public void sing() {
 		System.out.println("bas sings");
